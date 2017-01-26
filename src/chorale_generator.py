1eb8cf81c4da16b11773456a1856904c948fd6d3
Sam Goree <sgoree@oberlin.edu>
2017-01-26 14:08:56 -0500

    now using multiple generation passes

--- src/chorale_generator.py
+++ src/chorale_generator.py
@@ -2,6 +2,7 @@
 # scripts the process for generating a chorale from scratch (composition) and from a melody (reharmonization)
 
 from train import *
+from scipy.stats import truncnorm
 
 PPQ = 480 # pulses per quarter note -- a midi thing that specifies the length of a timestep
 
@@ -11,7 +12,7 @@ PPQ = 480 # pulses per quarter note -- a midi thing that specifies the length of
 # soprano controls whether the model expects a soprano voice to exist
 # output dir will be created if it is not supplied
 # visualize controls whether the training process will be visualized at validation
-def instantiate_and_train_bass_model(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, soprano=False, output_dir=None, visualize=False):
+def instantiate_and_train_bass_model(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, output_dir=None, visualize=False):
 	timestep_info = T.itensor3()
 	prior_timesteps=T.itensor4()
 	pieces=T.itensor4()
@@ -25,12 +26,16 @@ def instantiate_and_train_bass_model(dataset, min_num, max_num, timestep_length,
 
 
 	# instantiate expert models
-	if soprano: bass_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 3, 0,pieces=pieces, piece=piece, rng=rng)
+	bass_soprano_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 3, 0, pieces=pieces, piece=piece, rng=rng)
+	bass_alto_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 2, 0, pieces=pieces, piece=piece, rng=rng)
+	bass_tenor_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 1, 0, pieces=pieces, piece=piece, rng=rng)
+	spacing_multiexpert = MultiExpert([bass_soprano_spacing_expert, bass_alto_spacing_expert, bass_tenor_spacing_expert], 4, 0, min_num, max_num, timestep_length, rhythm_encoding_size,
+		pieces=pieces, prior_timesteps=prior_timesteps, timestep_info=timestep_info, piece=piece, rng=rng, transparent=True)
 	bass_contour_expert = VoiceContourExpert(min_num, max_num, [100,200,100], 0,
 		voices=voices, gen_length=gen_length, first_note=first_note, rng=rng)
 	bass_rhythm_expert = RhythmExpert(rhythm_encoding_size, max_num-min_num, [100,200,100], 0, 
 		timestep_info=timestep_info, prior_timestep_pitch_info=prior_timesteps, pieces=pieces, rhythm_info=rhythm_info, rng=rng)
-	models = [bass_spacing_expert, bass_contour_expert, bass_rhythm_expert] if soprano else [bass_contour_expert, bass_rhythm_expert]
+	models = [spacing_multiexpert, bass_contour_expert, bass_rhythm_expert]
 	bass_model = MultiExpert(models, 4, 0,  min_num, max_num, timestep_length, rhythm_encoding_size,
 		pieces=pieces, prior_timesteps=prior_timesteps, timestep_info=timestep_info, piece=piece, rng=rng, transparent=visualize)
 	train(bass_model, 'Bass_model', dataset, min_num, max_num, timestep_length, output_dir=output_dir, visualize=visualize)
@@ -50,12 +55,16 @@ def instantiate_and_train_soprano_model(dataset, min_num, max_num, timestep_leng
 
 
 	# instantiate chorale models
-	soprano_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 0, 3,pieces=pieces, piece=piece, rng=rng)
+	soprano_alto_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 2, 3, pieces=pieces, piece=piece, rng=rng)
+	soprano_tenor_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 1, 3, pieces=pieces, piece=piece, rng=rng)
+	soprano_bass_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 0, 3, pieces=pieces, piece=piece, rng=rng)
+	spacing_multiexpert = MultiExpert([soprano_alto_spacing_expert, soprano_tenor_spacing_expert, soprano_bass_spacing_expert], 4, 3, min_num, max_num, timestep_length, rhythm_encoding_size,
+		pieces=pieces, prior_timesteps=prior_timesteps, timestep_info=timestep_info, piece=piece, rng=rng, transparent=True)
 	soprano_contour_expert = VoiceContourExpert(min_num, max_num, [100,200,100], 3,
 		voices=voices, gen_length=gen_length, first_note=first_note, rng=rng)
 	soprano_rhythm_expert = RhythmExpert(rhythm_encoding_size, max_num-min_num, [100,200,100], 3, 
 		timestep_info=timestep_info, prior_timestep_pitch_info=prior_timesteps, pieces=pieces, rhythm_info=rhythm_info, rng=rng)
-	soprano_model = MultiExpert([soprano_spacing_expert, soprano_contour_expert, soprano_rhythm_expert], 4, 3,  min_num, max_num, timestep_length, rhythm_encoding_size,
+	soprano_model = MultiExpert([spacing_multiexpert, soprano_contour_expert, soprano_rhythm_expert], 4, 3,  min_num, max_num, timestep_length, rhythm_encoding_size,
 		pieces=pieces, prior_timesteps=prior_timesteps, timestep_info=timestep_info, piece=piece, rng=rng, transparent=visualize)
 	train(soprano_model, 'Soprano_model', dataset, min_num, max_num, timestep_length, output_dir=output_dir, visualize=visualize)
 	return soprano_model
@@ -77,8 +86,9 @@ def instantiate_and_train_alto_model(dataset, min_num, max_num, timestep_length,
 
 	# instantiate chorale models
 	alto_soprano_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 3, 2, pieces=pieces, piece=piece, rng=rng)
+	alto_tenor_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 1, 2, pieces=pieces, piece=piece, rng=rng)
 	alto_bass_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 0, 2, pieces=pieces, piece=piece, rng=rng)
-	spacing_multiexpert = MultiExpert([alto_soprano_spacing_expert, alto_bass_spacing_expert], 4, 2, min_num, max_num, timestep_length, rhythm_encoding_size,
+	spacing_multiexpert = MultiExpert([alto_soprano_spacing_expert, alto_tenor_spacing_expert, alto_bass_spacing_expert], 4, 2, min_num, max_num, timestep_length, rhythm_encoding_size,
 		pieces=pieces, prior_timesteps=prior_timesteps, timestep_info=timestep_info, piece=piece, rng=rng, transparent=True)
 	alto_contour_expert = VoiceContourExpert(min_num, max_num, [100,200,100], 2,
 		voices=voices, gen_length=gen_length, first_note=first_note, rng=rng)
@@ -107,7 +117,7 @@ def instantiate_and_train_tenor_model(dataset, min_num, max_num, timestep_length
 	tenor_soprano_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 3, 1, pieces=pieces, piece=piece, rng=rng)
 	tenor_alto_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 2, 1, pieces=pieces, piece=piece, rng=rng)
 	tenor_bass_spacing_expert = VoiceSpacingExpert(max_num-min_num, [100,200,100], 0, 1, pieces=pieces, piece=piece, rng=rng)
-	spacing_multiexpert = MultiExpert([tenor_soprano_spacing_expert, tenor_alto_spacing_expert, tenor_bass_spacing_expert], 3, 1, min_num, max_num, timestep_length, rhythm_encoding_size,
+	spacing_multiexpert = MultiExpert([tenor_soprano_spacing_expert, tenor_alto_spacing_expert, tenor_bass_spacing_expert], 4, 1, min_num, max_num, timestep_length, rhythm_encoding_size,
 		pieces=pieces, prior_timesteps=prior_timesteps, timestep_info=timestep_info, piece=piece, rng=rng, transparent=True)
 	tenor_contour_expert = VoiceContourExpert(min_num, max_num, [100,200,100], 1,
 		voices=voices, gen_length=gen_length, first_note=first_note, rng=rng)
@@ -118,40 +128,51 @@ def instantiate_and_train_tenor_model(dataset, min_num, max_num, timestep_length
 	train(tenor_model, 'Tenor_model', dataset, min_num, max_num, timestep_length, output_dir=output_dir, visualize=visualize)
 	return tenor_model
 
+# empty_voice should be a zero-initialized array for a voice of the correct length and correct note encoding
+# rng is a random number generator
+# generated voice will have a gaussian distribution with standard deviation 1/4 of the note encoding from the middle note of the encoding
+def generate_random_voice(empty_voice, rng):
+	mean = empty_voice.shape[1]//2
+	stddev = empty_voice.shape[1]//4
+	notes = np.floor(truncnorm.rvs(-2, 2, loc=mean, scale=stddev, size=empty_voice.shape[0], random_state=rng)) # this takes truncation bounds in terms of multiples of the standard deviation away from mean of 0
+	for i, note in enumerate(notes):
+		empty_voice[i][note] = 1
+
+
+
 # generate a piece from "scratch" (a random first note and piece length for the bass in the dataset)
 # dataset etc. are the same things I pass into everything
 # num_to_generate is the number of pieces to output (from different first notes)
 # soprano,alto,tenor,bass weights are string filepaths of pickled model files. If they are left as None, new models will be trained from the dataset
 # if visualize is true, the training process will be visualized at every validation step
 def generate_voice_by_voice(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, num_to_generate, soprano_weights=None, alto_weights=None, tenor_weights=None, bass_weights=None, visualize=False):
+	rng = np.random.RandomState()
 	# make output directory
 	output_dir = '../Data/Output/generate_voice_by_voice/' + strftime("%a,%d,%H:%M", localtime())+ '/'
 	if not os.path.exists('../Data/Output/generate_voice_by_voice'): os.mkdir('../Data/Output/generate_voice_by_voice')
 	os.mkdir(output_dir)
 	# train models
-	soprano_model = instantiate_and_train_soprano_model(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, output_dir=output_dir, visualize=visualize) if soprano_weights is None else load_weights(soprano_weights)
-	alto_model = instantiate_and_train_alto_model(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, output_dir=output_dir, visualize=visualize)if alto_weights is None else load_weights(alto_weights)
-	tenor_model = instantiate_and_train_tenor_model(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, output_dir=output_dir, visualize=visualize)if tenor_weights is None else load_weights(tenor_weights)
-	bass_model = instantiate_and_train_bass_model(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, output_dir=output_dir, visualize=visualize)if bass_weights is None else load_weights(bass_weights)
+	models = []
+	models.append(instantiate_and_train_bass_model(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, output_dir=output_dir, 
+		visualize=visualize)if bass_weights is None else load_weights(bass_weights))
+	models.append(instantiate_and_train_tenor_model(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, output_dir=output_dir, 
+		visualize=visualize)if tenor_weights is None else load_weights(tenor_weights))
+	models.append(instantiate_and_train_alto_model(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, output_dir=output_dir, 
+		visualize=visualize)if alto_weights is None else load_weights(alto_weights))
+	models.append(instantiate_and_train_soprano_model(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, output_dir=output_dir, 
+		visualize=visualize) if soprano_weights is None else load_weights(soprano_weights))
 
 	for i in range(num_to_generate):
-		# choose a random bach piece to steal the first note of
+		# choose a random bach piece to steal the length of
 
 		sample_piece = dataset[np.random.randint(len(dataset))]
 		generated_piece = np.zeros_like(sample_piece)
-		generated_piece[:,0] = sample_piece[:,0] # make the first note of all voices
-		# generate bass
-		bass_voice = bass_model.generate(generated_piece)
-		generated_piece[0] = bass_voice
-		# generate soprano
-		soprano_voice = soprano_model.generate(generated_piece)
-		generated_piece[3] = soprano_voice
-		# generate alto
-		alto_voice = alto_model.generate(generated_piece)
-		generated_piece[2] = alto_voice
-		# generate tenor
-		tenor_voice = tenor_model.generate(generated_piece)
-		generated_piece[1] = tenor_voice
+		for i in range(4):
+			generate_random_voice(generated_piece[i], rng)
+		for i in range(100): # arbitrary number of generation runs
+			v = int(np.floor(rng.uniform(0,4,1))[0])
+			# generate bass
+			generated_piece[v] = models[v].generate(generated_piece)
 
 		output_midi([timesteps_to_notes(voice, min_num, timestep_length * PPQ) for voice in generated_piece], path=output_dir + 'output' + str(i) + '.mid')
 
@@ -242,12 +263,7 @@ def harmonize_melody_and_bass(dataset, min_num, max_num, timestep_length, rhythm
 
 # load dataset
 if __name__ == '__main__':
-	paths = music21.corpus.getBachChorales()
-	dataset, min_num, max_num, timestep_length = load_dataset_music21(paths)
+	dataset, min_num, max_num, timestep_length = pickle.load(open('../Data/music21.dataset', 'rb'))
 	rhythm_encoding_size = int(4//timestep_length)
-	harmonize_melody_and_bass(dataset, min_num,  max_num, timestep_length, rhythm_encoding_size, 10)#, soprano_weights='../Data/Output/Soprano_model/Tue,17,09:18/320.p', alto_weights='../Data/Output/Alto_model/Tue,17,09:23/240.p',
-																			#tenor_weights='../Data/Output/Tenor_model/Tue,17,09:30/280.p', bass_weights='../Data/Output/Bass_Model/Tue,17,09:37/60.p')
-	harmonize_bass(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, 10)
-	harmonize_melody(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, 10)
 	generate_voice_by_voice(dataset, min_num, max_num, timestep_length, rhythm_encoding_size, 10)
 	
